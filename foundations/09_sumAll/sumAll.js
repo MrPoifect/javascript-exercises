e08f51c690a2287b9f3ddf0a32c394c1abe6d423
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-20 12:02:37 +0100

    Complete JS foundations - 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        let nums = [num1, num2];
+        nums.sort((a, b) => a - b);
+        let highNum = Math.max(...nums);
+        let lowNum = Math.min(...nums);
+        let allNums = [];
+
+        for (let i = lowNum; i <= highNum; i++) {
+            allNums.push(i)
+        }
+        const sum = allNums.reduce((acc, num) => acc + num, 0);
+        return sum
+    } else return "ERROR";
 
 };
 
