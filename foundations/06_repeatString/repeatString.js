3e99179dcd029647282777a8ce465cd0eea31b10
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-20 11:05:27 +0100

    Complete JS foundation-06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let mergedString = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            mergedString += string;
+        }
+        
+    }else {mergedString += "ERROR";}
+    return mergedString;
 
 };
 
