c85511ca90fad7a8525385ba94cc0c4bdac89820
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-27 14:28:01 +0100

    Complete JS foundation 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const number = Number(num);
+
+    if (number === 0 || number === 1) {
+        return number;
+    } else if (number < 0){
+        return "OOPS";
+    }
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (number === 1) {
+        return num1;
+    } else if (number === 2){
+        return num2;
+    } else {
+        for(let i = 2; i <= number; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 
 };
 
