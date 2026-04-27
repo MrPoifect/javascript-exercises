3cf0abb26f1a918318730749467d8d9313c72a69
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-27 14:10:04 +0100

    Complete JS foundations 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowstr = string.toLowerCase();
+    const noPunctuation = lowstr.match(/\w/g).join("");
+    return noPunctuation == noPunctuation.split('').reverse().join('');
 };
 
 // Do not edit below this line
