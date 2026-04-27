1bc13929de975a1f81ca6fdea11cfbcbf2fe00c8
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-27 15:23:04 +0100

    Complete JS foundation 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldestAge = 0;
+    let oldestPerson = "";
+    array.forEach(age => {
+        let tempAge = getAge(age.yearOfBirth, age.yearOfDeath);
+        if(tempAge > oldestAge){
+            oldestAge = tempAge
+            oldestPerson = age
+        }
+    });
+    return oldestPerson;
 };
+
+function getAge(birth, death) {
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
